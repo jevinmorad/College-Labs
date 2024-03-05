725369b19ce5f3e57938484bf9a6ddf2fd380e5f
jevinmorad <143516246+jevinmorad@users.noreply.github.com>
2024-03-05 20:01:01 +0530

    HTML files

--- java/Lab-1/CommandInput.java
+++
@@ -1,5 +0,0 @@
-public class CommandInput {
-    public static void main(String[] args) {
-        System.out.println("Inputs are "+args[0]+" "+args[1]);
-    }    
-}
--- java/Lab-1/commandSum.java
+++
@@ -1,7 +0,0 @@
-public class commandSum {
-    public static void main(String[] args) {
-        double a = Double.parseDouble(args[0]);
-        double b = Double.parseDouble(args[1]);
-        System.out.println("Sum is : "+(a+b));
-    }
-}
