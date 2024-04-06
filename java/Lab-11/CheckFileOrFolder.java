301cf46867f6dd1d82250b64b0c13e0eaa14e219
jevinmorad <23010101171@darshan.ac.in>
2024-04-06 08:50:29 +0530

    java files

--- java/Lab-11/CheckFileOrFolder.java
+++ java/Lab-11/CheckFileOrFolder.java
@@ -1,26 +1,26 @@
 import java.io.*;
+import java.lang.String;
 
 public class CheckFileOrFolder {
     public static void main(String[] args) {
         if (args.length != 1) {
-            System.out.println("Enter a valis file name");
+            System.out.println("Enter a valid file name in command line please.");
             return;
         } else {
             File file = new File(args[0]);
 
-            if (!file.exists()) {
-                System.err.println("File or directory not found: " + file.getAbsolutePath());
-            }
-            else {
-            if (file.isFile()) {
-                System.out.println("File size: " + file.length() + " bytes");
+            if (!file.isFile() && !file.isDirectory()) {
+                System.out.println("File or directory not found.");
             } else {
-                System.out.println("Directory contents:");
-                for (File child : file.listFiles()) {
-                    System.out.println(child.getName());
+                if (file.isFile()) {
+                    System.out.println("File size: " + file.length() + " bytes");
+                } else {
+                    System.out.println("Directory contents:");
+                    for (File child : file.listFiles()) {
+                        System.out.println(child.getName());
+                    }
                 }
             }
-        }
         }
     }
 }
