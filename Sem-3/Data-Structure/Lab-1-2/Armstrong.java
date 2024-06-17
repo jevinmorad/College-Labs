d32159c411a1b2f47a72512326defc08a78e32b1
jevinmorad <23010101171@darshan.ac.in>
2024-06-17 22:45:17 +0530

    Lab 1&2

--- Sem-3/Data-Structure/Lab-1-2/Armstrong.java
+++ Sem-3/Data-Structure/Lab-1-2/Armstrong.java
@@ -1,16 +1,18 @@
 public class Armstrong {
     public static void main(String[] args) {
         System.out.print("Armstrong numbers : ");
-        int len=1, n, sum=0;
-        for (int i = 0; i <= 1000; i++) {
+        int len=1, n;
+        for (int i = 1; i <= 1000; i++) {
             len = length(i);
             n=i;
+            int sum = 0;
             while (n>0) {
                 sum += Math.pow(n%10, len);
                 n/=10;
             }
-            if (sum==i)
+            if (sum==i) {
                 System.out.print(i+" ");
+            }
         }        
     }
     public static int length(int n) {
