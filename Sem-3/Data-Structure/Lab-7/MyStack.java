b46d9825a7b85a1821053bfbd2043da886f6101b
jevinmorad <23010101171@darshan.ac.in>
2024-07-09 19:38:19 +0530

    Lab-7

--- Sem-3/Data-Structure/Lab-7/MyStack.java
+++ Sem-3/Data-Structure/Lab-7/MyStack.java
@@ -1,11 +1,10 @@
 import java.util.Scanner;
 
-class MyStack<T> {
+public class MyStack<T> {
     T[] arr;
     int size;
     int top;
-    Scanner sc = new Scanner(System.in);
-
+    
     @SuppressWarnings("unchecked")
     public MyStack(int size) {
         this.size = size;
@@ -68,53 +67,49 @@ class MyStack<T> {
     public int size() {
         return top;
     }
-}
 
-public class StackClass {
+    public boolean isEmpty() {
+        return top==-1;
+    }
+    
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter size of stack : ");
         int n = sc.nextInt();
         boolean check = true;
 
-        MyStack<Integer> st = new MyStack<Integer>(n);
+        MyStack<Integer> st = new MyStack<>(n);
 
         while (check) {
             System.out.println("\n[1]push\n[2]pop\n[3]peep\n[4]display\n[5]change\n[6]Exit");
             System.out.print("\nEnter your choice : ");
             int ch = sc.nextInt();
             switch (ch) {
-                case 1:
+                case 1 -> {
                     System.out.print("\nEnter number to push : ");
                     int x = sc.nextInt();
                     st.push(x);
-                    break;
+                }
 
-                case 2:
-                    System.out.println("Poped number : " + st.pop());
-                    break;
+                case 2 -> System.out.println("Poped number : " + st.pop());
 
-                case 3:
+                case 3 -> {
                     System.out.print("Enter index : ");
                     int i = sc.nextInt();
                     System.out.println("Numer at index " + i + " : " + st.peep(i));
-                    break;
+                }
 
-                case 4:
-                    st.display();
-                    break;
+                case 4 -> st.display();
 
-                case 5:
+                case 5 -> {
                     System.out.print("Enter index to change number : ");
                     int ind = sc.nextInt();
                     System.out.println("Enter new number : ");
                     int val = sc.nextInt();
                     st.change(ind, val);
-                    break;
+                }
 
-                case 6:
-                    check = false;
-                    break;
+                case 6 -> check = false;
             }
         }
         sc.close();
