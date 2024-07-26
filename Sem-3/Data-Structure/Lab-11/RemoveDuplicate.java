186eee5c23c5813027c93682b23102cfdf743204
jevinmorad <23010101171@darshan.ac.in>
2024-07-26 19:57:37 +0530

    Lab-11

--- Sem-3/Data-Structure/Lab-11/RemoveDuplicate.java
+++ Sem-3/Data-Structure/Lab-11/RemoveDuplicate.java
@@ -1,7 +1,5 @@
 import java.util.Scanner;
 
-import Lab_10.MyLinkedList;
-
 public class RemoveDuplicate {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
