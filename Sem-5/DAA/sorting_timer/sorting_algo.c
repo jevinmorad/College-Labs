0aad25eb8cadf6fbc3d395f406568c4a49040951
Jevin Morad <143516246+jevinmorad@users.noreply.github.com>
2025-06-23 13:42:16 +0530

    complete selection sort

--- Sem-5/DAA/sorting_timer/sorting_algo.c
+++ Sem-5/DAA/sorting_timer/sorting_algo.c
@@ -1,15 +1,24 @@
 #include "sorting_algo.h"
+#include <stdbool.h>
 
 void bubble_sort(int *arr, int n)
 {
     for (int i = 0; i < n - 1; ++i)
+    {
+        bool isSwaped = false;
         for (int j = 0; j < n - i - 1; ++j)
+        {
             if (arr[j] > arr[j + 1])
             {
+                isSwaped = true;
                 int tmp = arr[j];
                 arr[j] = arr[j + 1];
                 arr[j + 1] = tmp;
             }
+            if (!isSwaped)
+                break;
+        }
+    }
 }
 
 void insertion_sort(int *arr, int n)
@@ -24,5 +33,26 @@ void insertion_sort(int *arr, int n)
             j--;
         }
         arr[j + 1] = key;
+    }
+}
+
+void selection_sort(int *arr, int n)
+{
+    for (int i = 0; i < n - 1; i++)
+    {
+        int min_ind = i;
+        for (int j = i + 1; j < n; j++)
+        {
+            if (arr[j] < arr[min_ind])
+            {
+                min_ind = j;
+            }
+        }
+        if (i != min_ind)
+        {
+            int temp = arr[min_ind];
+            arr[min_ind] = arr[i];
+            arr[i] = temp;
+        }
     }
 }
