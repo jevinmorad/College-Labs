a4db2879b1dc5ce11ad4518dba958cfe5e5e5520
jevinmorad <23010101171@darshan.ac.in>
2025-08-06 19:32:42 +0530

    static crud with IFOrmColletion

---
+++ Sem-5/Asp .NET Core/ICollection/Controllers/UserController.cs
@@ -0,0 +1,85 @@
+﻿using ICollection.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+using System.Collections.Generic;
+using System.IO;
+using System;
+
+namespace ICollection.Controllers
+{
+    public class UserController : Controller
+    {
+        private static List<UserModel> users = new List<UserModel>();
+
+        public IActionResult UserAddEdit()
+        {
+            ViewBag.Users = users;
+            return View(new UserModel());
+        }
+
+        #region WithMode
+        //[HttpPost]
+        //public IActionResult UserAddEdit(UserModel user, IFormFile ImageFile)
+        //{
+        //    if (ImageFile != null && ImageFile.Length > 0)
+        //    {
+        //        var uploadsFolder = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/images");
+        //        if (!Directory.Exists(uploadsFolder))
+        //            Directory.CreateDirectory(uploadsFolder);
+
+        //        var uniqueFileName = Guid.NewGuid().ToString() + Path.GetExtension(ImageFile.FileName);
+        //        var filePath = Path.Combine(uploadsFolder, uniqueFileName);
+
+        //        FileStream stream = new FileStream(filePath, FileMode.CreateNew);
+        //        ImageFile.CopyTo(stream);
+        //        stream.Close();
+
+        //        user.Image = "/images/" + uniqueFileName;
+        //    }
+        //    users.Add(user);
+
+        //    ViewBag.Users = users;
+        //    ModelState.Clear();
+        //    return View(new UserModel());
+        //}
+        #endregion
+
+        #region WithIFormCollection
+        [HttpPost]
+        public IActionResult UserAddEdit(IFormCollection fc)
+        {
+            var user = new UserModel
+            {
+                UserName = fc["UserName"].ToString(),
+                Email = fc["Email"].ToString(),
+                Password = fc["Password"].ToString(),
+                Age = int.TryParse(fc["Age"], out int age) ? age : 0,
+                Gender = fc["Gender"].ToString(),
+                Hobbies = fc["Hobbies"].ToString(),
+                Phone = fc["Phone"].ToString()
+            };
+            var ImageFile = fc.Files.GetFile("Image");
+            if (ImageFile != null && ImageFile.Length > 0)
+            {
+                var uploadsFolder = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/images");
+                if (!Directory.Exists(uploadsFolder))
+                    Directory.CreateDirectory(uploadsFolder);
+                
+                var uniqueFileName = Guid.NewGuid().ToString() + Path.GetExtension(ImageFile.FileName);
+                var filePath = Path.Combine(uploadsFolder, uniqueFileName);
+                
+                FileStream stream = new FileStream(filePath, FileMode.CreateNew);
+                ImageFile.CopyTo(stream);
+                stream.Close();
+                
+                user.Image = "/images/" + uniqueFileName;
+            }
+
+            users.Add(user);
+            ViewBag.Users = users;
+            ModelState.Clear();
+            return View(new UserModel());
+        }
+        #endregion
+    }
+}
---
+++ Sem-5/Asp .NET Core/ICollection/Models/UserModel.cs
@@ -0,0 +1,15 @@
+﻿namespace ICollection.Models
+{
+    public class UserModel
+    {
+        public int UserId { get; set; }
+        public string UserName { get; set; }
+        public string Image { get; set; }
+        public string Password { get; set; }
+        public int Age { get; set; }
+        public string Email { get; set; }
+        public string Gender { get; set; }
+        public string Hobbies { get; set; }
+        public string Phone { get; set; }
+    }
+}
